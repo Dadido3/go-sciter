b5a2c79a415d237f8d21fe17905457f1cfe2e487
David Vogel <Dadido3@aol.com>
2019-07-12 15:22:43 +0200

    Lock main function to main thread

--- window/window.go
+++ window/window.go
@@ -1,13 +1,19 @@
 package window
 
 import (
+	"runtime"
+
 	"github.com/sciter-sdk/go-sciter"
-	// "runtime"
 )
 
 type Window struct {
 	*sciter.Sciter
 	creationFlags sciter.WindowCreationFlag
+}
+
+func init() {
+	// Lock main function to main thread
+	runtime.LockOSThread()
 }
 
 func (w *Window) run() {
